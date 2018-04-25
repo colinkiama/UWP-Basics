732a341fe03de2daed5a36ceb977a4e73f11e4ae
Colin Kiama <colinkiama@hotmail.co.uk>
2018-04-25 14:21:07 +0100

    early beginnings

--- Basics/Examples/TextHierachy/BuiltInStyles.xaml.cs
+++ Basics/Examples/TextHierachy/BuiltInStyles.xaml.cs
@@ -13,23 +13,24 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Basics
+namespace Basics.Examples.TextHierachy
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class BuiltInStyles : Page
     {
-        public MainPage()
+        public BuiltInStyles()
         {
             this.InitializeComponent();
         }
 
-        private void TextHierachyButton_Click(object sender, RoutedEventArgs e)
+       
+        private void customStylePageButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Examples.TextHierachy.BuiltInStyles));
-        }
+
+            Frame.Navigate(typeof(CustomStyles));
     }
 }
